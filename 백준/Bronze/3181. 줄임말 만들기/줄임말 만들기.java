24295a01a5eb6e8c3b3042aef7990317608f1d0e
yoonseo-aa <yoonseo0084@gmail.com>
2025-04-06 17:04:55 +0900

    Update 줄임말 만들기.java

--- 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.java
+++ 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.java
@@ -4,18 +4,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();  // 입력 받기 (예: "i am a student and i love java")
+        String str = br.readLine(); (예: "i am a student and i love java")
 
         String[] tmp = {"i", "pa", "te", "ni", "niti", "a", "ali", "nego", "no", "ili"};
         List<String> tmpList = new ArrayList<>(Arrays.asList(tmp));
 
-        String[] words = str.split(" "); // 문자열을 단어 단위로 나누기
+        String[] words = str.split(" ");
         List<String> strList = new ArrayList<>(Arrays.asList(words));
 
         int i = 0;
         for (String s : strList) {
             if (i == 0 || !tmpList.contains(s)) {
-                System.out.print(Character.toUpperCase(s.charAt(0)));  // 한 글자만 대문자로 출력
+                System.out.print(Character.toUpperCase(s.charAt(0))); 
             }
             i++;
         }

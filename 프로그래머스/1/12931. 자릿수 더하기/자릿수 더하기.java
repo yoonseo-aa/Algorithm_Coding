b18142415702b2a852ce99a7381ffac54b919b2d
yoonseo-aa <yoonseo0084@gmail.com>
2025-10-31 23:48:07 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String s = String.valueOf(n);
+        for(int i=0; i < s.length(); i++){
+             answer += s.charAt(i)- '0';;
+        }
+        return answer;
+    }
+}

ab6a74e622b595a125b76b74dfd047d869855968
yoonseo-aa <yoonseo0084@gmail.com>
2025-11-16 22:43:31 +0900

    [level 1] Title: 예산, Time: 0.59 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int cnt = 0;
+        Arrays.sort(d);
+        for(int i=0;i<d.length;i++){
+            budget -= d[i];
+            if(budget < 0) break;
+            cnt++;
+        }
+        return cnt;
+    }
+}

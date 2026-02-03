f817a76056539b31e3a3a955d6e50f0d3b1638ed
yoonseo-aa <yoonseo0084@gmail.com>
2026-02-03 17:19:29 +0900

    Title: 따옴표 출력, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

--- 260203/따옴표 출력/print-quote.java
+++ 260203/따옴표 출력/print-quote.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.println("He says \"It's a really simple sentence.\".")
+        System.out.println("He says \"It's a really simple sentence\".");
     }
 }

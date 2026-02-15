4019060aaeb53f2c1e1dd8b11bacdd0db3b5084f
yoonseo-aa <yoonseo0084@gmail.com>
2026-02-15 23:19:42 +0900

    [Bronze I] Title: 딱지놀이, Time: 148 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.py
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -3,25 +3,16 @@ N = int(input())
 for _ in range(N):
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    if a[0] != b[0] and a[1:].count(4) != b[1:].count(4):
-        if a[1:].count(4) > b[1:].count(4):
+
+    A = a[1:]
+    B = b[1:]
+
+    for shape in [4, 3, 2, 1]:
+        if A.count(shape) > B.count(shape):
             print('A')
-        else:
+            break
+        elif A.count(shape) < B.count(shape):
             print('B')
-    elif a[1:].count(3) != b[1:].count(3):
-        if a[1:].count(3) < b[1:].count(3):
-            print('B')
-        else:
-            print('A')
-    elif a[1:].count(2) != b[1:].count(2):
-        if a[1:].count(2) < b[1:].count(2):
-            print('B')
-        else:
-            print('A')
-    elif a[1:].count(1) != b[1:].count(1):
-        if a[1:].count(1) < b[1:].count(1):
-            print('B')
-        else:
-            print('A')
+            break
     else:
         print('D')

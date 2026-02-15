046da62c2b2c8fc6f22d3b6a0eb4bd145b84e193
yoonseo-aa <yoonseo0084@gmail.com>
2026-02-15 20:50:36 +0900

    [Silver V] Title: 영화감독 숌, Time: 500 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+count = 0
+num = 666
+
+while True:
+    if "666" in str(num):
+        count += 1
+        if count == N:
+            print(num)
+            break
+    num += 1

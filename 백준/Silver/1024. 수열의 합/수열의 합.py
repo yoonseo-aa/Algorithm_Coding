d2294e3f225876275fd3ff1e4e4936b193b281e4
yoonseo-aa <yoonseo0084@gmail.com>
2026-03-17 23:29:37 +0900

    [Silver II] Title: 수열의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1024. 수열의 합/수열의 합.py
@@ -0,0 +1,38 @@
+N, L = map(int, input().split())
+
+result = []
+temp = 0
+
+while(1):
+    a = N//L
+    b = N%L
+    if (b == 0):
+        start = a - L//2
+        end = a + L//2
+    else:
+        start = a - b + 1
+        end = a + b
+
+    start = int((2*N-L*L+L)*(2*L)**(-1)) 
+    end = start + L -1
+
+    if (start < 0):
+        start += abs(start)
+        end += abs(start)
+
+    for i in range(start, end+1):
+        result.append(i)
+        temp += i
+
+    if(temp == N):
+        break
+    else:
+        result = []
+        temp = 0
+        L += 1
+        if (L > 100):
+            result.append(-1)
+            break
+
+for k in result:
+    print(k, end=" ")
